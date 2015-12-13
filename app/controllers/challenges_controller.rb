76c58bfc258659d375f9c023fbbd9a452280c9ac
ReemAlattas <ralattas@live.com>
2015-12-13 17:39:21 -0500

    change db to pg

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,4 +1,6 @@
 class ChallengesController < ApplicationController
+  protect_from_forgery with: :exception
+  before_action :authenticate_user! 
   before_action :set_challenge, only: [:show, :edit, :update, :destroy]
 
   # GET /challenges
